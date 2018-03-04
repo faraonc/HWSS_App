fa9841ea5ee1562b2af6e04824ec1b079155786e
faraonc <faraonc@outlook.com>
2018-03-03 17:22:34 -0800

    Do not use off-canvas.html, Use off-canvas2.html/css/js

---
+++ public/off_canvas2.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+
+    new Vue({
+        el: "#map-container",
+        data: {
+            map: null,
+            navState: true
+        },
+        mounted: function () {
+            var latLng = new google.maps.LatLng(47.6062, -122.3321);
+            var mapOptions = {
+                zoom: 3,
+                center: latLng,
+                mapTypeId: google.maps.MapTypeId.SATELLITE,
+
+                // disables the yellow man
+                panControl: false,
+                streetViewControl: false,
+
+                // disables other types of MAP, forces to only use Satellite view
+                mapTypeControlOptions: {mapTypeIds: []}
+
+            };
+            this.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+        },
+        methods: {
+            toggleMenu: function () {
+                this.navState = !this.navState;
+            }
+        }
+
+    });
+});
